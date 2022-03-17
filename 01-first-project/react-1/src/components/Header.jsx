481cc41af65f641c1fb4ae5365a87676f6f726a5
Oleg Kraev <Lega.WellnesS@gmail.com>
2022-03-17 22:08:58 +0200

    first commit

---
+++ 01-first-project/react-1/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Header.module.css';
+
+const Header = () => {
+    return <header className={classes.header}>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BMW_logo_%28gray%29.svg/2048px-BMW_logo_%28gray%29.svg.png' />
+    </header>
+}
+
+export default Header;
